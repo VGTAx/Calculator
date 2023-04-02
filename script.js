299fbc505961e553f69ea26d5effcebe977478cd
Mikhail Novikau <melmenx@gmail.com>
2023-04-02 15:10:28 +0300

    impl keydown events, add comment, fix bug

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 
-let first = '0';
-let second = '0';
-let sign = '';
-let result = false;
-let percent = '0'
-let digitAfterComma = 0;
-let digit = ['0','1','2', '3','4','5','6','7','8','9','.'];
-let signs = ['+','-','*','/'];
+let first = '0'; //first Number
+let second = '0'; //second Number
+let sign = ''; // sign
+let result = false; //The variable indicates that the math operation has completed. 
+let percent = 0; // the variable stores the value %
+let digitAfterComma = 0; // the varibale stores the count of digit after comma in 
+                        // float number. Its use for function toFixed()
+let keyValues = ['0','1','2', '3','4','5','6','7','8','9','.']; 
+let keyCodes = ['Digit0','Digit1', 'Digit2', 'Digit3', 'Digit4', 
+                  'Digit5','Digit6','Digit7', 'Digit8', 'Digit9', 
+                  'Numpad0', 'Numpad1','Numpad2','Numpad3','Numpad4',
+                  'Numpad5','Numpad6','Numpad7','Numpad8','Numpad9']; // key
+let signsValues = ['+','-','*','/'];
+let signsCodes = ['NumpadAdd','NumpadSubtract', 'NumpadDivide', 'NumpadMultiply'];
 let display = document.querySelector('.display p');
-
+//Clear all values 
 function Clear() {
   first = '0';
   second = '0';
@@ -17,7 +23,7 @@ function Clear() {
   result = false;
   display.textContent = '0';
 }
-
+//Clear current enter value
 function ClearEntry() {
   if(first !== '0' && sign === ''){
     display.textContent = 0;
@@ -26,15 +32,15 @@ function ClearEntry() {
   display.textContent = 0;
   return second = '0';
 }
-
+//Clear one digit of number
 function ClearOneDigit() {
-  if(fisrt !== '0' && second !=='0' && sign !== '') return;
+  if(first !== '0' && second !=='0' && result) return;
   if(first !== '0' && sign === ''){
     first = first.slice(0,-1);
-    return display.textContent = first;
+    return display.textContent = first.length == 0 ? '0' : display.textContent = first;
   }
   second = second.slice(0,-1);
-  display.textContent = second;
+  display.textContent = second.length > 0 ? display.textContent = second : 0;
 }
 //count number digit after comma for method toFixed();
 function NumberDigitAfterComma(first, second, sign) {
@@ -66,7 +72,7 @@ function NumberDigitAfterComma(first, second, sign) {
 
   return numberDigitFirst >= numberDigitSecond ? numberDigitFirst : numberDigitSecond;
 }
-
+//Checks for a dot in a number 
 function checkDigit(number, key) {
   if(number === '0') {
     if(key === '.') {
@@ -81,7 +87,7 @@ function checkDigit(number, key) {
     return number += key;
   }
 }
-
+//calc result
 function calcResult() {
   switch(sign) {
     case '+':
@@ -113,7 +119,7 @@ function calcResult() {
   display.textContent = first;
   result = true;
 }
-
+//reverses the sign of a number
 function signReversal(){
   if(second === '0' && sign === ''){
     first = -first;
@@ -136,11 +142,12 @@ function signReversal(){
     display.textContent = second;
   }    
 }
+//divide one to number
 function oneToShare() {
   if(second === '0' && sign === ''){
     first = 1/first;
     display.textContent = first;
-  }  
+  } 
   else if (first !== '0' && second !== '0' && sign !== '' && !result){
     second = 1/second;
     display.textContent = second;
@@ -149,6 +156,7 @@ function oneToShare() {
     first = 1/first;
     display.textContent = first;
   }  
+  // The result indicates that the math operation has completed. And when using the result in other calculations, the first number will be equal to the result, and the second will be equal to the entered value
   else if (first !== '0' && sign !== '' && result){      
     second = '0';
     result = false;
@@ -158,16 +166,18 @@ function oneToShare() {
     display.textContent = second;
   }   
 }
+//calc percent
 function calcPercent(){ 
   percent = second;
   second = +first * +percent/100 ;  
   return display.textContent = second; 
 }
+//calculates the square of a number
 function square(){
   if(second === '0' && sign === ''){
     first = Math.pow(first,2);
     display.textContent = first;
-  }  
+  }    
   else if (first !== '0' && second !== '0' && sign !== '' && !result){
     second = Math.pow(second,2);
     display.textContent = second;
@@ -176,6 +186,7 @@ function square(){
     first = Math.pow(first,2);
     display.textContent = first;
   }  
+  // The result indicates that the math operation has completed. And when using the result in other calculations, the first number will be equal to the result, and the second will be equal to the entered value
   else if (first !== '0' && sign !== '' && result){      
     second = Math.pow(first,2);
     result = false;
@@ -185,7 +196,7 @@ function square(){
     display.textContent = second;
   }   
 }
-
+//calculates the square root of a number
 function squareRoot() {
   if(second === '0' && sign === ''){
     first = Math.sqrt(first);
@@ -211,11 +222,12 @@ function squareRoot() {
 document.addEventListener('click', (event) => {
   if(!event.target.classList.contains('btn')) return
   let key = event.target.textContent;  
-  if(digit.includes(key)) {
+  if(buttonValues.includes(key)) {
     if(second === '0' && sign === ''){       
       first = checkDigit(first,key); 
       display.textContent = first;
     }
+    // The result indicates that the math operation has completed. And when using the result in other calculations, the first number will be equal to the result, and the second will be equal to the entered value
     else if (first !== '' && sign !== '' && result){      
       second = '0';
       second = checkDigit(second,key);
@@ -227,7 +239,7 @@ document.addEventListener('click', (event) => {
       display.textContent = second;
     }    
   }
-  if(signs.includes(key)){
+  if(signsValues.includes(key)){
     sign = key;
     display.textContent = sign;
   }
@@ -239,7 +251,7 @@ document.addEventListener('click', (event) => {
     case 'C':
       Clear();
       break;
-    case 'clr':
+    case '<=':
       ClearOneDigit();
       break;
     case '=':
@@ -255,6 +267,65 @@ document.addEventListener('click', (event) => {
       oneToShare();
       break;
     case 'x2':
+      square();
+      break;  
+    case '√X':
+      squareRoot();
+      break;
+  }
+})
+
+document.addEventListener('keydown', (event) => {
+  
+  let key = event.code == 'Digit5' && event.shiftKey ?
+            '%' : event.code; 
+  if(buttonValues.includes(key)) {
+    if(second === '0' && sign === ''){       
+      first = checkDigit(first,event.key); 
+      display.textContent = first;
+    }
+    // The result indicates that the math operation has completed. And when using the result in other calculations, the first number will be equal to the result, and the second will be equal to the entered value
+    else if (first !== '' && sign !== '' && result){      
+      second = '0';
+      second = checkDigit(second,event.key);
+      display.textContent = second;
+      result = false;
+    }
+    else {
+      second = checkDigit(second, event.key);
+      display.textContent = second;
+    }    
+  }
+  if(signsValues.includes(key)){
+    sign = event.key;
+    display.textContent = sign;
+  }  
+
+  switch(key) {
+    case 'Delete':
+      return !result ? ClearEntry() : null;
+    case 'Escape':
+      Clear();
+      break;
+    case 'Backspace':
+      ClearOneDigit();
+      break;
+    case 'NumpadEnter':
+      calcResult();
+      break;
+    case 'Enter':
+      calcResult();
+      break; 
+    case 'F9':
+      signReversal();
+      break;
+    case '%':
+      calcPercent();      
+      break;
+    case 'KeyR':
+      oneToShare();
+      break;
+    case 'KeyQ':
       square();
       break;  
     case '√X':
